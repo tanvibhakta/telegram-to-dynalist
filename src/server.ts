tanvibhakta/telegram-to-dynalist

commit 710069848abc41cba24a62703809e48676e72d52

edit an item when the message is edited even if the message has command `/add`

src/server.ts
@@ -85,11 +85,6 @@ app.post("/webhook", async (req: Request, res: Response) => {
 function getActionType(req: Request) {
 
   let actionType = ACTIONS.ADD;
-  if (req.body.message) {
-    actionType = ACTIONS.ADD;
-  } else if (req.body.edited_message) {
-    actionType = ACTIONS.EDIT;
-  }
 
   let message: string = req.body.message?.text || req.body.edited_message.text;
 
@@ -103,7 +98,11 @@ function getActionType(req: Request) {
     actionType = ACTIONS.DONE;
   }
 
-  //TODO: if "/add" but edited_message, set actionType to ACTION.EDIT anyway
+  if (req.body.message) {
+    actionType = ACTIONS.ADD;
+  } else if (req.body.edited_message) {
+    actionType = ACTIONS.EDIT;
+  }
 
   return actionType;
 }

commit 5bce15912cddc7dc95cd1c680e587f223a5428c3

Fix bug introduced in 710069848abc41cba24a62703809e48676e72d52

src/server.ts
@@ -96,11 +96,11 @@ function getActionType(req: Request) {
     actionType = ACTIONS.DELETE;
   }  else if (message?.startsWith("/done")) {
     actionType = ACTIONS.DONE;
+  } else if (message?.startsWith("/showAllTasks")) {
+    actionType = ACTIONS.SHOW_ALL_TASKS;
   }
 
-  if (req.body.message) {
-    actionType = ACTIONS.ADD;
-  } else if (req.body.edited_message) {
+    if ( actionType == ACTIONS.ADD && req.body.edited_message) {
     actionType = ACTIONS.EDIT;
   }
 